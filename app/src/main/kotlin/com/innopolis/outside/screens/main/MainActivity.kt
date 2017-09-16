87d7fa9299690cf08fccfc4dc1adbd1423d97437
“SergeyPinkevich” <sergeipinkevich163@gmail.com>
2017-09-16 22:35:19 +0300

    layout interim

--- app/src/main/kotlin/com/innopolis/outside/screens/main/MainActivity.kt
+++ app/src/main/kotlin/com/innopolis/outside/screens/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.innopolis.outside.screens.main
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.innopolis.outside.R
+import com.innopolis.outside.common.BaseApp
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), MainView {
@@ -13,5 +14,12 @@ class MainActivity : AppCompatActivity(), MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        BaseApp.Companion.component?.inject(this)
+        presenter.attachView(this)
+    }
+
+    override fun showMessage(message: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- app/src/main/kotlin/com/innopolis/outside/screens/main/MainPresenter.kt
+++ app/src/main/kotlin/com/innopolis/outside/screens/main/MainPresenter.kt
@@ -1,6 +1,9 @@
 package com.innopolis.outside.screens.main
 
+import com.innopolis.outside.common.network.Service
 import com.innopolis.outside.screens.core.BasePresenter
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
 import javax.inject.Singleton
 
 /**
@@ -10,9 +13,15 @@ import javax.inject.Singleton
 class MainPresenter : BasePresenter<MainView> {
 
     var view: MainView? = null
+    var disposable: CompositeDisposable? = null
+    var service: Service? = null
 
-    override fun attachView(v: MainView) {
-        view = v
+    @Inject constructor(service: Service) {
+        this.service = service
+    }
+
+    override fun attachView(view: MainView) {
+        this.view = view
     }
 
     override fun detachView() {
